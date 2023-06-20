fd63a74bb8ddff8fb83b02270ca67bf132b4de13
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-06-20 18:23:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
 
+let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'The Rolling Stones', 'Aerosmith'];
+
+function sortBandNames(bandNames) {
+  // Remove articles ('a', 'an', 'the') from band names
+  let sortedBandNames = bandNames.map(name => name.replace(/^(a|an|the)\s+/i, ''));
+
+  // Sort the band names in lexicographic order
+  sortedBandNames.sort((a, b) => a.localeCompare(b));
+
+  return sortedBandNames;
+}
+
+// Get the ul element with id 'band'
+let ulElement = document.getElementById('band');
+
+// Sort and display the band names inside the ul element
+let sortedBandNames = sortBandNames(bandNames);
+sortedBandNames.forEach(name => {
+  let liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});

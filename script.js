41e98f7d40239a0c48cb9383b4ccd1d8f77ac6d6
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-07-13 10:50:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-//your code here
-
-let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'The Rolling Stones', 'Aerosmith'];
-
-function sortBandNames(bandNames) {
-  // Remove articles ('a', 'an', 'the') from band names
-  let sortedBandNames = bandNames.map(name => name.replace(/^(a|an|the)\s+/i, ''));
-
-  // Sort the band names in lexicographic order
-  sortedBandNames.sort((a, b) => a.localeCompare(b));
-
-  return sortedBandNames;
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
 }
-
-// Get the ul element with id 'band'
-let ulElement = document.getElementById('band');
-
-// Sort and display the band names inside the ul element
-let sortedBandNames = sortBandNames(bandNames);
-sortedBandNames.forEach(name => {
-  let liElement = document.createElement('li');
-  liElement.textContent = name;
-  ulElement.appendChild(liElement);
-});
+}
+return s.trim()
+}
